622bef857d0c655af153cebdba985cc0a47b63c4
FerPa <fpalazuelo@gmail.com>
2017-05-01 21:04:00 +0200

    Revert "Revert "añadiendo proyecto c#""
    This reverts commit ae49c71e3fd3bcd6e9984eaff5a6270dc23a8e84.

---
+++ sdi3-23.Cli-SOAPcsharp/sdi3-23.Cli-SOAPcsharp/ListarUsuariosAction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sdi3_23.Cli_SOAPcsharp
+{
+    class ListarUsuariosAction
+    {
+        public void execute()
+        {
+            long id;
+            try
+            {
+                id = 
+            }
+        }
+
+
+
+    }
+}
