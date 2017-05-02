385d8911f8771be725863c19a292c861e1e402c8
FerPa <fpalazuelo@gmail.com>
2017-05-02 22:12:46 +0200

    cliente c# con javaUtil

---
+++ sdi3-23.Cli-SOAPcsharp/sdi3-23.Cli-SOAPcsharp/MainMenu.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using alb.util.console;
+using alb.util.menu;
+using alb.util.log;
+
+namespace sdi3_23.Cli_SOAPcsharp
+{
+    class MainMenu
+    {
+        protected List<string> opciones = new List<string>();
+        protected List<Accion> acciones = new List<Accion>();
+        String titulo;
+        public MainMenu()
+        {
+            
+            titulo = "Menú administrador EJB";            
+
+            opciones.Add("Listar usuarios");
+            acciones.Add(new ListarUsuariosAction());
+
+            opciones.Add("Eliminar usuarios");
+            acciones.Add(new EliminarUsuariosAccion());
+
+            opciones.Add("Deshabilitar usuarios");
+            acciones.Add(new DeshabilitarUsuariosAction());
+
+        }
+        
+        static void Main(string[] args)
+        {
+            Log.setLogLevel(LogLevel.DEBUG);
+
+            new MainMenu().execute();
+          
+        }
+
+        public void execute()
+        {
+            int opcion = -1;
+            do
+            {
+                pintarMenu();
+                try
+                {
+                    opcion = Consola.readInt().intValue();
+                    if (opcion > 0)
+                    {
+                        acciones[opcion - 1].execute();
+                    }
+                }catch(NullReferenceException nre)
+                {
+                    Log.trace("el usuario ha seleccionado una opción inválida");
+                    Log.trace(nre);
+                    opcion = -1;
+                }
+
+            } while (opcion != 0);
+        }
+
+        private void pintarMenu()
+        {
+            Consola.println(titulo);
+            int i = 1;
+            foreach(string opcion in opciones)
+            {
+                
+                Consola.printf("\t %s - %s \n",i.ToString(),opcion);
+                ++i;
+            }
+
+            Consola.println("\t 0 - Salir");
+        }
+    }
+}
